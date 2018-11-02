1da421d750a20afe3468285e861a4ffbb70ff4e4
samzoll22 <sam.zoll22@gmail.com>
2018-11-01 20:45:20 -0700

    dickstein/marram color update

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -30,7 +30,7 @@ class App extends React.Component {
     return (
     <div className="spacer">
       <Jumbotron className="background">
-        <h1 className="center" style={{ fontFamily: 'Permanent Marker', fontSize: '8em', color: '#fff' }}>i believe them</h1>
+        <h1 className="center" style={{ fontFamily: 'Permanent Marker', fontSize: '8em', color: '#000' }}>i believe them</h1>
         <h3 className="center" style={{ fontFamily: 'Lobster', fontSize: '3em' }}>
           Under Construction!
         </h3>
@@ -38,8 +38,8 @@ class App extends React.Component {
           Full Site Coming Soon!
         </h3>
         <p>
-          <Button style={{ margin: '10px' }} bsStyle="success" bsSize="large" ><a style={{ color: '#fff' }} href="http://secure.actblue.com/donate/ibelievethem" target="_blank" rel="noopener noreferrer">Donate</a></Button>
-          <Button style={{ margin: '10px' }} bsStyle="info" bsSize="large" ><a style={{ color: '#fff' }} href="http://www.instagram.com/ibelievethem" target="_blank" rel="noopener noreferrer">Instagram</a></Button>
+          <Button style={{ margin: '10px' }} bsStyle="success" bsSize="large" ><a style={{ color: '#000' }} href="http://secure.actblue.com/donate/ibelievethem" target="_blank" rel="noopener noreferrer">Donate</a></Button>
+          <Button style={{ margin: '10px' }} bsStyle="info" bsSize="large" ><a style={{ color: '#000' }} href="http://www.instagram.com/ibelievethem" target="_blank" rel="noopener noreferrer">Instagram</a></Button>
         </p>
       </Jumbotron>
     </div>)
