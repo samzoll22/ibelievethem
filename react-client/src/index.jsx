403f513cb30fd589b986f40ff3ce01c4d237dcc7
samzoll22 <sam.zoll22@gmail.com>
2018-11-01 20:40:49 -0700

    first build on s3

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
+import { Button, Jumbotron } from 'react-bootstrap';
+// import $ from 'jquery';
+// import List from './components/List.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,24 +12,36 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
-  }
+  // componentDidMount() {
+  //   $.ajax({
+  //     url: '/items', 
+  //     success: (data) => {
+  //       this.setState({
+  //         items: data
+  //       })
+  //     },
+  //     error: (err) => {
+  //       console.log('err', err);
+  //     }
+  //   });
+  // }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+    return (
+    <div className="spacer">
+      <Jumbotron className="background">
+        <h1 className="center" style={{ fontFamily: 'Permanent Marker', fontSize: '8em', color: '#fff' }}>i believe them</h1>
+        <h3 className="center" style={{ fontFamily: 'Lobster', fontSize: '3em' }}>
+          Under Construction!
+        </h3>
+        <h3 className="center" style={{ fontFamily: 'Lobster', fontSize: '3em' }}>
+          Full Site Coming Soon!
+        </h3>
+        <p>
+          <Button style={{ margin: '10px' }} bsStyle="success" bsSize="large" ><a style={{ color: '#fff' }} href="http://secure.actblue.com/donate/ibelievethem" target="_blank" rel="noopener noreferrer">Donate</a></Button>
+          <Button style={{ margin: '10px' }} bsStyle="info" bsSize="large" ><a style={{ color: '#fff' }} href="http://www.instagram.com/ibelievethem" target="_blank" rel="noopener noreferrer">Instagram</a></Button>
+        </p>
+      </Jumbotron>
     </div>)
   }
 }
