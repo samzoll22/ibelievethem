094f2b01fd08c718c25f541f3ce3fcd607059cb2
samzoll22 <sam.zoll22@gmail.com>
2018-11-11 22:28:44 -0800

    adding basic working serverless

---
+++ server/schema.js
@@ -0,0 +1,36 @@
+const schema = `
+type Author {
+    id: ID! # the ! means that every author object _must_ have an id
+    firstName: String
+    lastName: String
+    """
+    the list of Posts by this author
+    """
+    posts: [Post]
+  }
+  
+  type Post {
+    id: ID!
+    title: String
+    author: Author
+    message: String
+  }
+  
+  # the schema allows the following query:
+  type Query {
+    posts: [Post]
+  }
+  
+  # this schema allows the following mutation:
+  type Mutation {
+    savePost 
+  }
+  
+  # we need to tell the server which types represent the root query
+  # and root mutation types. We call them RootQuery and RootMutation by convention.
+  schema {
+    query: Query
+    mutation: Mutation
+  }`
+
+export default schema;
